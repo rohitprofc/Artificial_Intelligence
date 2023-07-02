847871dcf521f20cf1a7e0710c446f8829803fb2
rohitprofc <rohitprofc@gmail.com>
2023-07-02 17:09:16 +0530

    2.0

--- Lab Programs/1-DFS.py
+++ Lab Programs/1-DFS.py
@@ -1,45 +1,26 @@
-adj_list = {"A": ["B","D","E"],
-            "B": ["B","D","E"],
-            "C": ["B","D","E"],
-            "D": ["B","D","E"],
-            "E": ["B","D","E"],
-            "F": ["B","D","E"],
-            "G": ["B","D","E"]}
-color = dict()
-parent = dict()
-trav_time = dict()
-stack = []
-for node in adj_list.keys():
-    color[node] = "W"
-    parent[node] = None
-    trav_time[node] = [-1,-1]
+def dfs(graph, start):
+    visited = set()  
+    stack = [start]  
 
-time = 0
-def dfs_util(node):
-    global time
-    color[node] = "G"
-    trav_time[node][0]=time
-    stack.append(node)
-    time+=1
-    for v in adj_list[node]:
-        if color[v] == "W":
-            parent[v]=node
-            dfs_util(v)
-    color[node]="B"
-    trav_time[node][1]=time
-    time+=1
-dfs_util("A")
-print("\nColor of each node: ",color)
-print("\nTravel time of each node: ",trav_time)
-print("\nParent of each node: ",parent)
-print("\nDFS Path: ",stack)
+    while stack:
+        node = stack.pop()  
+
+        if node not in visited:
+            visited.add(node)
+            print(node)  
+
+            stack.extend(graph[node] - visited)
+
+    return visited
+
+graph = {"A": {"B","D","E"},
+            "B": {"B","D","E"},
+            "C": {"B","D","E"},
+            "D": {"B","D","E"},
+            "E": {"B","D","E"},
+            "F": {"B","D","E"},
+            "G": {"B","D","E"}}
 
 
-#Output:-
-# Color of each node:  {'A': 'B', 'B': 'B', 'C': 'W', 'D': 'B', 'E': 'B', 'F': 'W', 'G': 'W'}
-
-# Travel time of each node:  {'A': [0, 7], 'B': [1, 6], 'C': [-1, -1], 'D': [2, 5], 'E': [3, 4], 'F': [-1, -1], 'G': [-1, -1]}
-
-# Parent of each node:  {'A': None, 'B': 'A', 'C': None, 'D': 'B', 'E': 'D', 'F': None, 'G': None}
-
-# DFS Path:  ['A', 'B', 'D', 'E']
+print("DFS Traversal:")
+dfs(graph, 'A')
