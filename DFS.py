569fd7aa3e4e7c49d0be13c0250d7c204359fded
rohitprofc <rohitprofc@gmail.com>
2023-07-02 12:17:04 +0530

    0.5

--- DFS.py
+++ DFS.py
@@ -35,12 +35,11 @@ print("\nParent of each node: ",parent)
 print("\nDFS Path: ",stack)
 
 
-#Output:- 
-
+#Output:-
 # Color of each node:  {'A': 'B', 'B': 'B', 'C': 'W', 'D': 'B', 'E': 'B', 'F': 'W', 'G': 'W'}
 
 # Travel time of each node:  {'A': [0, 7], 'B': [1, 6], 'C': [-1, -1], 'D': [2, 5], 'E': [3, 4], 'F': [-1, -1], 'G': [-1, -1]}
 
 # Parent of each node:  {'A': None, 'B': 'A', 'C': None, 'D': 'B', 'E': 'D', 'F': None, 'G': None}
 
-# DFS Path:  ['A', 'B', 'D', 'E'] 
+# DFS Path:  ['A', 'B', 'D', 'E']
