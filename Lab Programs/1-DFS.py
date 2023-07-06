71e7a7c270220d6393f12972a44313381daef2be
rohitprofc <rohitprofc@gmail.com>
2023-07-06 20:06:37 +0530

    2.0

--- Lab Programs/1-DFS.py
+++ Lab Programs/1-DFS.py
@@ -14,13 +14,23 @@ def dfs(graph, start):
     return visited
 
 graph = {"A": {"B","D","E"},
-            "B": {"B","D","E"},
-            "C": {"B","D","E"},
-            "D": {"B","D","E"},
-            "E": {"B","D","E"},
-            "F": {"B","D","E"},
-            "G": {"B","D","E"}}
+            "B": {"C","E","A"},
+            "C": {"B","E","F","G"},
+            "D": {"A","E"},
+            "E": {"A","B","C","D","F"},
+            "F": {"C","E","G"},
+            "G": {"C","F"}}
 
 
 print("DFS Traversal:")
 dfs(graph, 'A')
+
+# Output:-
+# DFS Traversal:
+# A
+# B
+# C
+# E
+# F
+# G
+# D
